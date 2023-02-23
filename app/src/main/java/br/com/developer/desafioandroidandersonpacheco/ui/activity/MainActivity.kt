0f3fb6349fb400fb957fe8961dc6413f0e4357dd
810943 <arpacheco@apoioprodesp.sp.gov.br>
2023-02-23 11:20:54 -0300

    teste

--- app/src/main/java/br/com/developer/desafioandroidandersonpacheco/ui/activity/MainActivity.kt
+++ app/src/main/java/br/com/developer/desafioandroidandersonpacheco/ui/activity/MainActivity.kt
@@ -29,6 +29,8 @@ class MainActivity : AppCompatActivity() {
         )
     }
 
+    // teste
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
